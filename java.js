b064cc3498959ea6285b492b0f5741aa3c834cac
davron20005 <128698498+davron20005@users.noreply.github.com>
2023-04-25 12:32:12 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,31 @@
+// var nameElement = document.getElementById("name")
+// var isValidElement = document.getElementById("isValid")
+// var formElement = document.getElementById("form")
+
+// nameElement.addEventListener("keyup", (evt) => {
+//     var values = evt.target.value;
+//     if (values.length > 4 && values.length < 20) {
+//         isValidElement.textContent = "is success"
+//         isValidElement.style.color = "green"
+//     }
+//     else if (values.length < 4) {
+//         isValidElement.textContent = "error"
+//         isValidElement.style.color = "red"
+//     }
+// })
+// formElement.addEventListener("submit", (event) => {
+//     event.preventDefault()
+//     if (nameElement.value.length < 4) { nameElement.focus() }
+// })
+
+
+
+//  var qizilElement = document.getElementById("qizl")
+//  var qoraElement = document.getElementById("qora")
+
+// qizilElement.addEventListener("click",()=>
+// {document.body.style.background = "red"})
+
+// qoraElement.addEventListener("click",()=>{
+//     document.body.style.background = "black"
+// })
